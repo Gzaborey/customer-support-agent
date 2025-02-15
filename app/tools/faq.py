3b34da317acf0dca8d32176724d5888e9ec3f764
George Zaborey <gzaborey@gmail.com>
2025-02-15 22:36:11 +0200

    feat: refactored tools

---
+++ app/tools/faq.py
@@ -0,0 +1,14 @@
+from langchain_core.tools import tool
+from app.common.utils import initialize_retriever
+
+
+retriever = initialize_retriever()
+
+@tool
+def get_faq_info(query: str) -> str:
+    """
+    Retrieve relevant documents from the Chroma vector store based on a query.
+    """
+    documents = retriever.invoke(query)
+    faq_info = "\n".join([document.page_content for document in documents])
+    return faq_info
---
+++ app/tools/order_customization.py
@@ -0,0 +1,65 @@
+from langchain_core.tools import tool
+from langgraph.prebuilt import InjectedState
+from app.common.utils import is_valid_customization_attribute, is_valid_customization_attribute_value, get_valid_shirt_attributes, get_valid_shirt_attribute_values
+from typing import Annotated, Union
+import random
+
+
+def get_current_shirt_specifications(state: Annotated[dict, InjectedState]) -> str:
+    return (
+        f"Current shirt customization setup: "
+        "".join(f"{attribute}: {value}" for attribute, value in state["order"].items())
+        )
+
+def validate_order_customization_input(attribute: str, value: str) -> Union[str, None]:
+    if not is_valid_customization_attribute(attribute):
+        available_options = ",".join(get_valid_shirt_attributes())
+        return f"Sorry, we don't have such an option. Available options for customization are {available_options}."
+
+    if not is_valid_customization_attribute_value(attribute, value):
+        available_options = ",".join(get_valid_shirt_attribute_values(attribute))
+        return f"Sorry, we don't have such an option. Available options for customizing the {attribute} are {available_options}."
+    
+@tool
+def customize_order(attribute: str, value: str, state: Annotated[dict, InjectedState]) -> str:
+    """
+    Updates a T-shirt customization based on the provided customization request.
+    Checks if there are remaining attributes to choose and guides the user accordingly.
+    """
+    if not is_valid_customization_attribute(attribute):
+        available_options = ",".join(get_valid_shirt_attributes())
+        return f"Sorry, we don't have such an option. Available options for customization are {available_options}."
+
+    if not is_valid_customization_attribute_value(attribute, value):
+        available_options = ",".join(get_valid_shirt_attribute_values(attribute))
+        return f"Sorry, we don't have such an option. Available options for customizing the {attribute} are {available_options}."
+
+    state["order"][attribute] = value
+
+    remaining_attributes = [attribute for attribute, value in state["order"].items() if value is None]
+
+    if not remaining_attributes:
+        shirt_specifications = ", ".join(f"{attribute}: {value}" for attribute, value in state["order"].items())
+        return (
+            f"Customization is successful! The {attribute} was set to {value}.\n"
+            f"The shirt customization is complete: {shirt_specifications}."
+            )
+
+    next_attribute = random.choice(remaining_attributes)
+    available_values = get_valid_shirt_attribute_values(next_attribute)
+    current_shirt_specifications = get_current_shirt_specifications(state)
+    return (
+        f"Customization is successful! The {attribute} was set to {value}.\n"
+        f"Please choose a value for {next_attribute}. Available options are: {available_values}.\n"
+        f"{current_shirt_specifications}"
+        )
+
+@tool
+def check_order_status(state: Annotated[dict, InjectedState]) -> str:
+    """
+    Checks the status of the shirt customization order.
+    """
+    return get_current_shirt_specifications(state)
+
+
+
